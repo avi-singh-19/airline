f31a7b933d55320b0e975b71fb54a3f3b3999c36
avi_singh <avi.v.singh@gmail.com>
2023-09-03 16:58:08 +0100

    Second commit, airport start

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Flight(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
+
+# python3 manage.py makemigrations (starts migration)
+# python3 manage.py migrate
+
